a184f9fe9ec0dea4769e09aa8de450af2454fb32
Diego Henrique <diego.teixeira@pdcase.com.br>
2025-09-17 16:57:47 -0300

    Busca de tópico pelo id e ajuste para transformar esses tópicos fixos em um atributo na memória

--- src/main/kotlin/br/com/alura/forum/service/TopicoService.kt
+++ src/main/kotlin/br/com/alura/forum/service/TopicoService.kt
@@ -7,9 +7,12 @@ import org.springframework.stereotype.Service
 import java.util.*
 
 @Service
-class TopicoService {
-    fun listar(): List<Topico> {
-        val topico = Topico(
+class TopicoService(private var topicos: List<Topico>) {
+
+
+    // dados em memória
+    init {
+        val topico1 = Topico(
             id = 1,
             titulo = "Dúvida Kotlin",
             mensagem = "Variaveis no Kotlin",
@@ -25,7 +28,51 @@ class TopicoService {
             )
         )
 
-        return Arrays.asList(topico, topico, topico)
+        val topico2 = Topico(
+            id = 2,
+            titulo = "Dúvida Kotlin 2",
+            mensagem = "Variaveis no Kotlin 2",
+            curso = Curso(
+                id = 1,
+                nome = "Kotlin",
+                categoria = "Programação"
+            ),
+            autor = Usuario(
+                id = 1,
+                nome = "Fulano da silva",
+                email = "fulano@meial.com"
+            )
+        )
+
+        val topico3 = Topico(
+            id = 3,
+            titulo = "Dúvida Kotlin 3",
+            mensagem = "Variaveis no Kotlin 3",
+            curso = Curso(
+                id = 1,
+                nome = "Kotlin",
+                categoria = "Programação"
+            ),
+            autor = Usuario(
+                id = 1,
+                nome = "Fulano da silva",
+                email = "fulano@meial.com"
+            )
+        )
+
+        topicos = Arrays.asList(topico1, topico2, topico3)
     }
+
+    fun listar(): List<Topico> {
+        return topicos
+    }
+
+    fun buscarPorId(id: Long): Topico {
+
+        return topicos.stream().filter({
+            t -> t.id == id
+        }).findFirst().get()
+    }
+
 
 }
